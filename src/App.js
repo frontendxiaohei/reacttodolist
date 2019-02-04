a0613cfa850a6c5e8d9181ae0d5bdd17a968538e
klsr <2112778581@qq.com>
2019-02-04 10:43:34 +0800

    to-do-list

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Todo from './components/todo/index.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Todo/>
+      </div>
+    );
+  }
+}
+
+/*
+    React.createElement(
+      "div",
+      null,
+      React.createElement(Todo, null)
+    );
+
+    var todo = new Todo()
+    return todo.render()
+*/
+
+export default App;
